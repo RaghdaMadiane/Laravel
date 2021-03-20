e3ac61cc4625493154db0a0ac6b0d8d90024bda7
RaghdaMadiane <raghdamadiane@gmail.com>
2021-03-20 17:06:55 +0200

    first lab

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $allPosts = [
+            ['id' => 1, 'title' => 'laravel', 'posted_by' => 'Ahmed', 'created_at' => '2021-03-20'],
+            ['id' => 2, 'title' => 'PHP', 'posted_by' => 'Mohamed', 'created_at' => '2021-04-15'],
+            ['id' => 3, 'title' => 'Javascript', 'posted_by' => 'Ali', 'created_at' => '2021-06-01'],
+        ];
+
+        return view('posts.index', [
+            'posts' => $allPosts
+        ]);
+    }
+
+    public function show($postId)
+    {
+        $post = ['id' => 1, 'title' => 'laravel', 'description' => 'laravel is awsome framework', 'posted_by' => 'Ahmed', 'created_at' => '2021-03-20','email'=> 'ahmed@gmail.com'];
+
+        return view('posts.show', [
+            'post' => $post,
+        ]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+    public function edit()
+    {
+        return view('posts.edit');
+    }
+
+
+    public function store()
+    {
+        return redirect()->route('posts.index');
+    }
+}
